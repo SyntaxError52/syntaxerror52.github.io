484e8ced80a47f43565ba2bf24ecd0e8a97d9f18
SyntaxError52 <160362207+SyntaxError52@users.noreply.github.com>
2024-08-25 09:53:25 -0500

    Fix

--- index.js
+++ index.js
@@ -12,3 +12,23 @@ function bio(len){
     };
 };
 
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+document.querySelector('h1').onmouseover = event => {
+    let iterations = 0;
+
+    const interval = setInterval( () => {
+        event.target.innerText = event.target.innerText.split('')
+        .map((letter, index) => {
+            if (index < iterations) {
+                return event.target.dataset.value[index];
+            }
+            
+            return letters[Math.floor(Math.random() * 26)]
+        }).join('');
+
+        if (iterations > 12) {clearInterval(interval)};
+        
+        iterations++;
+    }, 30)
+};
